69b1956d37027dfb9265de2f6829507c09071a9a
Cory Brzycki <supercclank@gatech.edu>
2016-03-12 18:18:37 -0500

    files updated

--- HorseMusicThing/app/src/main/java/com/example/ihavenoidea/myapplication/MainActivity.java
+++ HorseMusicThing/app/src/main/java/com/example/ihavenoidea/myapplication/MainActivity.java
@@ -8,29 +8,29 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.app.Activity;
-import android.content.res.AssetFileDescriptor;
 import android.media.AudioManager;
 import android.media.SoundPool;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     TextView textView;
-    StringBuilder builder = new StringBuilder();
     StringBuilder keyBuilder = new StringBuilder();
     SoundPool sp;
+    boolean moveTaken = false;
     int soundfile;
     HashMap<String, String> buttonMapping = new HashMap<String, String>();
+    int count = 0;
+    SensorEvent oldEvent = null;
+
     String lastDirection = "NONE";
 
     float [] history = new float[3];
-    String[] direction = {"NONE", "NONE", "NONE"};
+    int[] direction = {0};
     String[] pressed = {"0", "0", "0"};
+    int[] oldDirection = {0};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,119 +41,83 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor accelerometer = manager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
         manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
 
-        buttonMapping.put("Left|000", "1");
-        buttonMapping.put("Left|001", "2");
-        buttonMapping.put("Left|010", "3");
-        buttonMapping.put("Left|011", "4");
-        buttonMapping.put("Left|100", "5");
-        buttonMapping.put("Left|101", "6");
-        buttonMapping.put("Left|110", "7");
-        buttonMapping.put("Left|111", "8");
-        buttonMapping.put("Right|000", "9");
-        buttonMapping.put("Right|001", "10");
-        buttonMapping.put("Right|010", "11");
-        buttonMapping.put("Right|011", "12");
-        buttonMapping.put("Right|100", "13");
-        buttonMapping.put("Right|101", "14");
-        buttonMapping.put("Right|110", "15");
-        buttonMapping.put("Right|111", "16");
+        buttonMapping.put("000", "1");
+        buttonMapping.put("001", "2");
+        buttonMapping.put("010", "3");
+        buttonMapping.put("011", "4");
+        buttonMapping.put("100", "5");
+        buttonMapping.put("101", "6");
+        buttonMapping.put("110", "7");
+        buttonMapping.put("111", "8");
 
         sp = new SoundPool(1,AudioManager.STREAM_MUSIC,0);
         soundfile = sp.load(this, R.raw.airhorn, 0);
     }
 
+    long lastUpdate = -1;
+    int rCount = 0;
+    int lCount = 0;
+    int zCount = 0;
     @Override
     public void onSensorChanged(SensorEvent event) {
-
-        //sp = new SoundPool(1,AudioManager.STREAM_MUSIC,0);
-        //soundfile = sp.load("/location/of/airhorn.m4a",0);
-
+        long currTime = System.currentTimeMillis();
+        if (currTime - lastUpdate < 100) {
+            lastUpdate = currTime;
+            return;
+        }
         float xChange = history[0] - event.values[0];
-        float yChange = history[1] - event.values[1];
-        float zChange = history[2] - event.values[2];
         history[0] = event.values[0];
-        history[1] = event.values[1];
-        history[2] = event.values[2];
 
-        if (xChange > 1){
-            direction[0] = "Left";
-        }
-        else if (xChange < -1){
-            direction[0] = "Right";
+        if (xChange > 3) {
+            direction[0] = -1;
+        } else if (xChange < -3) {
+            direction[0] = 1;
         }
 
-        if (yChange > 1){
-            direction[1] = "Towards";
-        }
-        else if (yChange < -1){
-            direction[1] = "Away";
-        }
-
-        if (zChange < -5) {
-            direction[2] = "Above";
-        }
-        else if (zChange > 5) {
-            direction[2] = "Below";
-        }
 
         if (findViewById(R.id.greenButton).isPressed()) {
             pressed[0] = "1";
-            //if (soundfile != 0){
-                //sp.play(soundfile, 1,1,0,0,1f); // play at original frequency
-            //}
-
         } else {
             pressed[0] = "0";
         }
         if (findViewById(R.id.redButton).isPressed()) {
             pressed[1] = "1";
-            //if (soundfile != 0){
-                //sp.play(soundfile, 1,1,0,0,2f); // play at 2x frequency
-            //}
         } else {
             pressed[1] = "0";
         }
         if (findViewById(R.id.yellowButton).isPressed()) {
             pressed[2] = "1";
-            //if (soundfile != 0){
-                //sp.play(soundfile, 1,1,0,0,3f); // play at 3x frequency
-            //}
         } else {
             pressed[2] = "0";
         }
-        
-        xChange = Math.abs(xChange);
-        yChange = Math.abs(yChange);
-        zChange = Math.abs(zChange);
 
-        builder.setLength(0);
-        builder.append("x: ");
-        builder.append(direction[0]);
-        builder.append(",    y: ");
-        builder.append(direction[1]);
-        builder.append(",    z: ");
-        builder.append(direction[2]);
-        builder.append("\n");
-        builder.append("Green: ");
-        builder.append(pressed[0]);
-        builder.append(",    Red: ");
-        builder.append(pressed[1]);
-        builder.append(",    Yellow: ");
-        builder.append(pressed[2]);
-
-        textView.setText(builder.toString());
-
-        keyBuilder.setLength(0);
-        if (xChange >= yChange && (xChange > 1 || xChange < -1) ) {
-            lastDirection = direction[0];
+        if (direction[0] == 1) {
+            rCount++;
+        }
+        if (direction[0] == -1) {
+            lCount++;
         }
 
-        keyBuilder.append(lastDirection);
-        keyBuilder.append("|");
-        keyBuilder.append(pressed[0]);
-        keyBuilder.append(pressed[1]);
-        keyBuilder.append(pressed[2]);
-        textView.setText(keyBuilder.toString());
+        if (rCount > 100) {
+            moveTaken = true;
+            oldDirection[0] = 1;
+            lCount = 0;
+        }
+        else if (lCount > 100) {
+            rCount = 0;
+            oldDirection[0] = -1;
+            moveTaken = true;
+        } else {
+            moveTaken = false;
+        }
+
+    if (moveTaken && oldDirection[0] != direction[0]) {
+        oldDirection[0] = 0;
+        rCount = 0;
+        lCount = 0;
+        //playSound
+        //pressed[0] + pressed[1] + pressed[2]//
+    }
     }
 
     @Override
