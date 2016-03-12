647d4c181d9b229e5807ffac065164896aced559
Cory Brzycki <supercclank@gatech.edu>
2016-03-12 15:14:12 -0500

    update to display correctly

--- HorseMusicThing/app/src/main/java/com/example/ihavenoidea/myapplication/MainActivity.java
+++ HorseMusicThing/app/src/main/java/com/example/ihavenoidea/myapplication/MainActivity.java
@@ -17,15 +17,20 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     TextView textView;
     StringBuilder builder = new StringBuilder();
+    StringBuilder keyBuilder = new StringBuilder();
     SoundPool sp;
     int soundfile;
+    HashMap<String, String> buttonMapping = new HashMap<String, String>();
+    String lastDirection = "NONE";
 
     float [] history = new float[3];
     String[] direction = {"NONE", "NONE", "NONE"};
-    String[] pressed = {"NO", "NO", "NO"};
+    String[] pressed = {"0", "0", "0"};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +40,30 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         Sensor accelerometer = manager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
         manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+
+        buttonMapping.put("Left|000", "1");
+        buttonMapping.put("Left|001", "2");
+        buttonMapping.put("Left|010", "3");
+        buttonMapping.put("Left|011", "4");
+        buttonMapping.put("Left|100", "5");
+        buttonMapping.put("Left|101", "6");
+        buttonMapping.put("Left|110", "7");
+        buttonMapping.put("Left|111", "8");
+        buttonMapping.put("Right|000", "9");
+        buttonMapping.put("Right|001", "10");
+        buttonMapping.put("Right|010", "11");
+        buttonMapping.put("Right|011", "12");
+        buttonMapping.put("Right|100", "13");
+        buttonMapping.put("Right|101", "14");
+        buttonMapping.put("Right|110", "15");
+        buttonMapping.put("Right|111", "16");
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        sp = new SoundPool(1,AudioManager.STREAM_MUSIC,0);
-        soundfile = sp.load("/location/of/airhorn.m4a",0);
+        //sp = new SoundPool(1,AudioManager.STREAM_MUSIC,0);
+        //soundfile = sp.load("/location/of/airhorn.m4a",0);
 
         float xChange = history[0] - event.values[0];
         float yChange = history[1] - event.values[1];
@@ -50,51 +72,56 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         history[1] = event.values[1];
         history[2] = event.values[2];
 
-        if (xChange > 2){
+        if (xChange > 1){
             direction[0] = "Left";
         }
-        else if (xChange < -2){
+        else if (xChange < -1){
             direction[0] = "Right";
         }
 
-        if (yChange > 2){
-            direction[1] = "Away";
-        }
-        else if (yChange < -2){
+        if (yChange > 1){
             direction[1] = "Towards";
         }
+        else if (yChange < -1){
+            direction[1] = "Away";
+        }
 
-        if (zChange < -2) {
+        if (zChange < -5) {
             direction[2] = "Above";
         }
-        else if (zChange > 2) {
+        else if (zChange > 5) {
             direction[2] = "Below";
         }
 
         if (findViewById(R.id.greenButton).isPressed()) {
-            pressed[0] = "YES";
-            if (soundfile != 0){
-                sp.play(soundfile, 1,1,0,0,1f); // play at original frequency
-            }
+            pressed[0] = "1";
+            //if (soundfile != 0){
+                //sp.play(soundfile, 1,1,0,0,1f); // play at original frequency
+            //}
         } else {
-            pressed[0] = "NO";
+            pressed[0] = "0";
         }
         if (findViewById(R.id.redButton).isPressed()) {
-            pressed[1] = "YES";
-            if (soundfile != 0){
-                sp.play(soundfile, 1,1,0,0,2f); // play at 2x frequency
-            }
+            pressed[1] = "1";
+            //if (soundfile != 0){
+                //sp.play(soundfile, 1,1,0,0,2f); // play at 2x frequency
+            //}
         } else {
-            pressed[1] = "NO";
+            pressed[1] = "0";
         }
         if (findViewById(R.id.yellowButton).isPressed()) {
-            pressed[2] = "YES";
-            if (soundfile != 0){
-                sp.play(soundfile, 1,1,0,0,3f); // play at 3x frequency
-            }
+            pressed[2] = "1";
+            //if (soundfile != 0){
+                //sp.play(soundfile, 1,1,0,0,3f); // play at 3x frequency
+            //}
         } else {
-            pressed[2] = "NO";
+            pressed[2] = "0";
         }
+
+        xChange = Math.abs(xChange);
+        yChange = Math.abs(yChange);
+        zChange = Math.abs(zChange);
+
 
         builder.setLength(0);
         builder.append("x: ");
@@ -112,6 +139,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         builder.append(pressed[2]);
 
         textView.setText(builder.toString());
+
+        keyBuilder.setLength(0);
+        if (xChange >= yChange && (xChange > 1 || xChange < -1) ) {
+            lastDirection = direction[0];
+        }
+
+        keyBuilder.append(lastDirection);
+        keyBuilder.append("|");
+        keyBuilder.append(pressed[0]);
+        keyBuilder.append(pressed[1]);
+        keyBuilder.append(pressed[2]);
+        textView.setText(keyBuilder.toString());
     }
 
     @Override
