ef3661551b25c59e78875b0c6a8b902a45d48fec
Cory Brzycki <supercclank@gatech.edu>
2016-03-12 12:55:22 -0500

    added the gyro code, let's see if it works

--- HorseMusicThing/app/src/main/java/com/example/ihavenoidea/myapplication/MainActivity.java
+++ HorseMusicThing/app/src/main/java/com/example/ihavenoidea/myapplication/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.ihavenoidea.myapplication;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    TextView textView;
+    StringBuilder builder = new StringBuilder();
+
+    float [] history = new float[2];
+    String[] direction = {"NONE","NONE"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        textView = new TextView(this);
+        setContentView(textView);
+
+        SensorManager manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelerometer = manager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+        manager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        float xChange = history[0] - event.values[0];
+        float yChange = history[1] - event.values[1];
+
+        history[0] = event.values[0];
+        history[1] = event.values[1];
+
+        if (xChange > 2){
+            direction[0] = "LEFT";
+        }
+        else if (xChange < -2){
+            direction[0] = "RIGHT";
+        }
+
+        if (yChange > 2){
+            direction[1] = "DOWN";
+        }
+        else if (yChange < -2){
+            direction[1] = "UP";
+        }
+
+        builder.setLength(0);
+        builder.append("x: ");
+        builder.append(direction[0]);
+        builder.append(" y: ");
+        builder.append(direction[1]);
+
+        textView.setText(builder.toString());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // nothing to do here
     }
 }
